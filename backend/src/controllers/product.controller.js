e3db707c385cddeaad7b06a3756e679270263245
prince pawar <princepawar891@gmail.com>
2026-04-08 12:14:34 +0530

    product-save-db

--- backend/src/controllers/product.controller.js
+++ backend/src/controllers/product.controller.js
@@ -1,27 +1,43 @@
-const product = require('../models/product.model.js')
+const product = require("../models/product.model.js");
 
-async function createProduct(req, res){
-    const { name, description, price, imageUrl, countInStock } = req.body;
-    if(!name || !description || !price){
-        return res.status(400).json({
-            message: "please fill all the fields"
-        })
+async function createProduct(req, res) {
+  const { name, description, price, imageUrl, countInStock } = req.body;
+  try {
+    if (!name || !description || !price) {
+      return res.status(400).json({
+        message: "please fill all the fields",
+      });
     }
     const productsave = new product({
-        name,
-        description,
-        price,
-        imageUrl,
-        countInStock
-    })
+      name,
+      description,
+      price,
+      imageUrl,
+      countInStock,
+    });
+    await productsave.save();
+    res.status(201).json({
+      message: "product created successfully",
+      product: productsave,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
 
-// await productsave.save();
-    await res.status(201).json({
-    message: "product created successfully",
-    product: productsave
-   })
-    
+async function getProducts(req, res) {
+  try {
+    const products = await product.find();
+    await res.status(200).json({
+      message: "products fetched successfully",
+      products: products,
+    });
+  } catch (error) {
+    console.log(error);
+  }
 }
+
 module.exports = {
-    createProduct
-}
+  createProduct,
+  getProducts,
+};
--- backend/src/routes/product.routes.js
+++ backend/src/routes/product.routes.js
@@ -2,4 +2,5 @@ const router = require('express').Router()
 
 const controller = require('../controllers/product.controller.js')
 router.post('/products', controller.createProduct)
+router.get('/products', controller.getProducts)
 module.exports = router;
